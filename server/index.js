c10815cbd3f1ceda7e79c7d6aae26b2862010d72
Sasibhushan Upadrasta <supadrasta@gmail.com>
2023-04-22 14:59:11 +0530

    implementation complete

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const express = require("express");
+const verifyProof = require("../utils/verifyProof");
 
 const port = 1225;
 
@@ -8,18 +8,26 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT =
+  "ddd59a2ffccddd60ff47993312821cd57cf30f7f14fb82937ebe2c4dc78375aa";
 
-app.post('/gift', (req, res) => {
+app.post("/gift", (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  // console.log(JSON.stringify(body));
+  // console.log(body.proof);
+  // console.log(body.name);
+  // console.log(body.root);
+
+  // TODO: prove that a name is in the list
+  const isInTheList = verifyProof(body.proof, body.name, MERKLE_ROOT)
+    ? true
+    : false;
+
+  if (isInTheList) {
     res.send("You got a toy robot!");
-  }
-  else {
+  } else {
     res.send("You are not on the list :(");
   }
 });
